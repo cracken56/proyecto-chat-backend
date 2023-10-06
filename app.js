418feac0d43289bb8cd9384a32dacc82832f37f5
Adrià Sierra <adriasierra@gmail.com>
2023-10-06 11:00:44 +0200

    Tidy up firestore implementation

--- app.js
+++ app.js
@@ -2,8 +2,19 @@ const express = require('express');
 const cors = require('cors');
 const { Firestore } = require('@google-cloud/firestore');
 
-// Initialize Firestore client without project configuration
-const firestore = new Firestore();
+// Initialize Firestore client
+async function initializeFirestore() {
+  try {
+    const firestore = new Firestore();
+    return firestore;
+  } catch (error) {
+    console.error('Firestore initialization error:', error);
+    throw error;
+  }
+}
+
+// Call the initialization function and handle any potential errors
+const firestore = await initializeFirestore();
 
 const app = express();
 const port = process.env.PORT || 3001;
@@ -22,10 +33,20 @@ const corsOptions = {
 
 app.use(cors());
 
-app.get('/api/sse/health', async (req, res) => {
+app.get('/api/health', async (req, res) => {
   res.status(200).send();
 });
 
-// Add a document to Firestore
-const newDocRef = firestore.collection('conversations').doc();
-await newDocRef.set({ name: 'John Doe' });
+// Route to add a document to Firestore
+app.get('/api/add-document', async (req, res) => {
+  try {
+    // Add a document to Firestore
+    const newDocRef = firestore.collection('conversations').doc();
+    await newDocRef.set({ name: 'John Doe' });
+
+    res.status(200).json({ message: 'Document added to Firestore' });
+  } catch (error) {
+    console.error('Error adding document to Firestore:', error);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+});
