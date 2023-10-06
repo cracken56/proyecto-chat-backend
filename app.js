5341589d3ea1ec1571581dc77810110d92ad4a02
Adrià Sierra <adriasierra@gmail.com>
2023-10-06 20:44:05 +0200

    Now a conversation is created when both users add each other. Also only allow validated users to send a message to a specific conversation

--- app.js
+++ app.js
@@ -16,12 +16,16 @@ const server = app.listen(port, () =>
 server.keepAliveTimeout = 120 * 1000;
 server.headersTimeout = 120 * 1000;
 
-const corsOptions = {
-  origin: 'https://chat.onrender.com',
-  optionsSuccessStatus: 200,
-};
+// const corsOptions = {
+//   origin: 'https://chat.onrender.com',
+//   optionsSuccessStatus: 200,
+// };
 
 app.use(cors(), bodyParser.json());
+
+app.get('/api/health', async (req, res) => {
+  res.status(200).send();
+});
 
 app.post('/api/register', async (req, res) => {
   try {
@@ -61,6 +65,7 @@ const validateUser = async (req, res, next) => {
     const hashedPassword = userDoc.data().hashedPassword;
 
     if (password === hashedPassword) {
+      req.user = user;
       next();
     } else {
       return res.status(401).json({ error: 'Unauthorized' });
@@ -73,23 +78,26 @@ const validateUser = async (req, res, next) => {
 
 app.use(validateUser);
 
-app.get('/api/health', async (req, res) => {
-  res.status(200).send();
-});
-
 app.post('/api/message', async (req, res) => {
   try {
     const { conversationId, message } = req.body;
 
-    const conversationDoc = firestore
+    const conversationDocRef = firestore
       .collection('conversations')
       .doc(conversationId);
 
-    const conversationSnapshot = await conversationDoc.get();
-    const conversationData = conversationSnapshot.data();
+    const conversationDoc = await conversationDocRef.get();
+    const conversationData = conversationDoc.data();
 
     if (!conversationData) {
       res.status(404).json({ success: false, error: 'Conversation not found' });
+      return;
+    }
+
+    // Check if the user sending the message is a participant in the conversation
+    const participants = conversationData.participants || [];
+    if (!participants.includes(req.user)) {
+      res.status(403).json({ success: false, error: 'Unauthorized' });
       return;
     }
 
@@ -98,7 +106,7 @@ app.post('/api/message', async (req, res) => {
     }
     conversationData.messages.push(message);
 
-    await conversationDoc.set(conversationData);
+    await conversationDocRef.set(conversationData);
 
     res.status(200).json({
       success: true,
@@ -107,50 +115,6 @@ app.post('/api/message', async (req, res) => {
   } catch (error) {
     console.error('Error sending message:', error);
     res.status(500).json({ success: false, error: 'Error sending message' });
-  }
-});
-
-// Endpoint that is called when the user clicks on a contact for the first time
-app.post('/api/conversation', async (req, res) => {
-  try {
-    const { participants } = req.body;
-
-    const existingConversationQuery = firestore
-      .collection('conversations')
-      .where('participants', 'array-contains', ...participants)
-      .get();
-
-    // Check if any matching conversations exist
-    const existingConversations = (await existingConversationQuery).docs;
-
-    if (existingConversations.length > 0) {
-      const existingConversation = existingConversations[0].data();
-
-      res.status(200).json({
-        success: true,
-        message: 'Conversation already exists',
-        conversation: existingConversation,
-      });
-    } else {
-      // Create a new conversation if none exists
-      const conversationDoc = firestore.collection('conversations').doc();
-      await conversationDoc.set({
-        conversationId: conversationDoc.id,
-        participants,
-      });
-
-      res.status(201).json({
-        success: true,
-        message: 'Conversation created successfully',
-        conversation: { conversationId: conversationDoc.id, participants },
-      });
-    }
-  } catch (error) {
-    console.error('Error creating or checking conversation:', error);
-    res.status(500).json({
-      success: false,
-      error: 'Error creating or checking conversation',
-    });
   }
 });
 
@@ -191,6 +155,38 @@ app.post('/api/contact/request', async (req, res) => {
   }
 });
 
+const createConversation = async (participants, res, message) => {
+  const existingConversationQuery = firestore
+    .collection('conversations')
+    .where('participants', 'array-contains', ...participants)
+    .get();
+
+  // Check if any matching conversations exist
+  const existingConversations = (await existingConversationQuery).docs;
+
+  if (existingConversations.length > 0) {
+    const existingConversation = existingConversations[0].data();
+    res.status(200).json({
+      success: true,
+      message: `${message}, but conversation already exists`,
+      conversation: existingConversation,
+    });
+  } else {
+    // Create a new conversation if none exists
+    const conversationDoc = firestore.collection('conversations').doc();
+    await conversationDoc.set({
+      conversationId: conversationDoc.id,
+      participants,
+    });
+
+    res.status(201).json({
+      success: true,
+      message: `${message}, and conversation created successfully`,
+      conversation: { conversationId: conversationDoc.id, participants },
+    });
+  }
+};
+
 // This is called when the user accepts the request
 app.post('/api/contact/accept-request/', async (req, res) => {
   try {
@@ -228,10 +224,11 @@ app.post('/api/contact/accept-request/', async (req, res) => {
     });
     await contactDocRef.update({ contacts: contactContacts });
 
-    res.status(200).json({
-      success: true,
-      message: 'Contact added successfully',
-    });
+    createConversation(
+      [user, contactToAccept],
+      res,
+      'Contact added successfully'
+    );
   } catch (error) {
     console.error('Error adding contact:', error);
     res.status(500).json({ success: false, error: 'Error adding contact' });
